cd0305496c693aac9eea9a6488ec9d8e4d62a377
EricScottRichards <ericscottrichards@gmail.com>
2015-05-20 20:11:34 -0600

    finished step 2.

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,5 +1,20 @@
 var app = angular.module('parseQ');
 app.controller('mainCtrl', function($scope, parseService){
 	// $scope.test = ''
+	$scope.question = ''
+
+	$scope.postData = function(){
+		console.log($scope.question)
+		parseService.postData($scope.question)
+			.then(function(data){
+				console.log(data)
+			})
+	}
+
+	// $scope.headers = function(){
+	// 	parseService.interceptor()
+	// }
+
+	// $scope.headers();
 	
 })
--- js/services/parseService.js
+++ js/services/parseService.js
@@ -1,4 +1,25 @@
 var app = angular.module('parseQ');
-app.service('parseService', function(){
+app.service('parseService', function($http, httpRequestInterceptor){
 	
+	// this.interceptor = function(){
+	// 	httpRequestInterceptor.request()
+	// 	.then(function(data){
+	// 		console.log(data)
+	// 	})
+	// }
+
+
+	this.postData = function(question){
+		console.log(question)
+		return $http(httpRequestInterceptor.request({
+			method: "POST",
+			url: "https://api.parse.com/1/classes/stuff",
+			data: {
+				text: question,
+				// 'X-Parse-Application-Id': 'EAnOuc7USiFJPsb7E9PHRumj5ohQ1pjEQpxPRn2z',
+				// 'X-Parse-REST-API-Key': 'Hm6ibRIKbFKxfwVtVsGVAaP6haLCMuw7wla5AMik'
+			}
+		}))
+	}
+
 })
